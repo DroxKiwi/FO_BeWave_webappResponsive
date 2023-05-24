6cdb1a9558a2a399bf75917452132e8e65ceb440
olivier <olivier@factoryz.fr>
2023-05-24 16:46:25 +0200

    Map fix + list enhance

--- bewave/src/components/search/List.js
+++ bewave/src/components/search/List.js
@@ -31,41 +31,29 @@ export default function ListView({setview}) {
 
 
     return (
-        <div className={"text-white min-h-[100vh] pt-20"}>
+        <div className={"text-white min-h-[100vh] py-20"}>
             <div className={"fixed flex flex-row justify-center self-center top-10 w-full"}>
                 <button
-                    className={"bg-gray-200 w-40 font-bold text-black rounded-l-xl"}
+                    className={"bg-gray-200 w-32 font-bold text-black rounded-l-xl"}
                     onClick={() => {
                         setview("map")
-                        window.scrollTo(0, 0)
                     }}>
                     Carte
                 </button>
                 <button
-                    className={"bg-orange-500 w-40 font-bold rounded-r-xl"}
+                    className={"bg-orange-500 w-32 font-bold rounded-r-xl"}
                     onClick={() => setview("list")}>
                     Liste
                 </button>
             </div>
             {Array.isArray(events) && events?.map(event => {
                 return <div className={"mx-5 my-10"}>
-                    <img src={event.image ? event.image : musicPlaceholder} className="h-[200px] m-auto" alt="logo"/>
+                    <img src={event.image ? event.image : musicPlaceholder} className="h-[200px] object-cover w-full m-auto mb-3" alt="logo"/>
                     <h2 className={"font-bold text-2xl"}>{event.name}</h2>
                     <p className={"text-gray-400 "}>Lieu</p>
                     <span className={"font-bold flex flex-row text-lg"}>
-                        <p className={"border-orange-500 outline-4"}>début - fin</p>
-                        <p className={"text-gray-300 mx-1"}>|</p>
-                        <p className={"text-orange-500"}>{event.price ? event.price + ' €' : 'Gratuit'}</p>
-                    </span>
-                </div>
-            })}
-            {Array.isArray(events) && events?.map(event => {
-                return <div className={"mx-5 my-10"}>
-                    <img src={event.image ? event.image : musicPlaceholder} className="h-[200px] m-auto" alt="logo"/>
-                    <h2 className={"font-bold text-2xl"}>{event.name}</h2>
-                    <p className={"text-gray-400 "}>Lieu</p>
-                    <span className={"font-bold flex flex-row text-lg"}>
-                        <p className={"border-orange-500 outline-4"}>début - fin</p>
+                        <p className={"border-orange-500 outline-4"}>
+                            {new Date(event.start_date).toLocaleDateString('fr-fr', {  month:"short", day:"numeric"}) } - {new Date(event.end_date).toLocaleDateString('fr-fr', {  month:"short", day:"numeric"}) }</p>
                         <p className={"text-gray-300 mx-1"}>|</p>
                         <p className={"text-orange-500"}>{event.price ? event.price + ' €' : 'Gratuit'}</p>
                     </span>
--- bewave/src/components/search/Map.js
+++ bewave/src/components/search/Map.js
@@ -4,20 +4,22 @@ import {Map, Marker} from "pigeon-maps"
 export default function MapView({setview}) {
 
     return (
-        <Map height={2000} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-            <div className={"flex flex-row justify-center mt-10"}>
+        <Map height={2000} defaultCenter={[47.3, -0.563166]} defaultZoom={12}>
+            <div className={"fixed flex flex-row justify-center self-center top-10 w-full"}>
                 <button
-                    className={"bg-orange-500 text-white w-40 font-bold rounded-l-xl"}
+                    className={"bg-orange-500 text-white w-32 font-bold rounded-l-xl"}
                     onClick={() => setview("map")}>
                     Carte
                 </button>
                 <button
-                    className={"bg-gray-200 w-40 font-bold rounded-r-xl"}
+                    className={"bg-gray-200 w-32 font-bold rounded-r-xl"}
                     onClick={() => setview("list")}>
                     Liste
                 </button>
             </div>
-            <Marker width={50} anchor={[50.879, 4.6997]}/>
+            <Marker width={50} color={"black"} anchor={[47.478419, -0.563166]}/>
+            <Marker width={50} color={"black"} anchor={[47.47, -0.55]}/>
+            <Marker width={50} color={"black"} anchor={[47.45, -0.53]}/>
         </Map>
 
     );
