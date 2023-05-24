7b065266eca6a735457be808547f183eb726b181
olivier <olivier@factoryz.fr>
2023-05-24 14:43:01 +0200

    Event List v2

--- bewave/src/components/search/List.js
+++ bewave/src/components/search/List.js
@@ -1,22 +1,44 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import musicPlaceholder from "../../assets/music-placeholder.png";
 
 export default function ListView({setview}) {
 
-    let events = {}
+    const [events, setEvents] = useState({})
 
-    fetch("http://backendv2.bewaveofficial.com/api/getevents")
-        .then(response => response.json())
-        .then(data => {
-            events = data
-            console.log(events)
-        })
+    useEffect(() => {
+        const getEvents = async () => {
+            const response = await fetch("http://backendv2.bewaveofficial.com/api/getevents")
+            const jsonData = await response.json()
+            setEvents(jsonData)
+        }
+
+        getEvents()
+    })
+
+    // STRUCTURE :
+
+    // artists:{}
+    // author_name:"admin"
+    // description:"Un évènement à ne pas manquer"
+    // display_map:true
+    // end_date:"2023-05-30T00:00:00.000Z"
+    // event_id:1
+    // externalmedias:{}
+    // images:{}
+    // name:"Super évènement"
+    // price:10
+    // start_date:"2023-05-05T00:00:00.000Z"
+
 
     return (
-        <div className={"text-white min-h-[100vh]"}>
-            <div className={"flex flex-row justify-center mt-10"}>
+        <div className={"text-white min-h-[100vh] pt-20"}>
+            <div className={"fixed flex flex-row justify-center self-center top-10 w-full"}>
                 <button
                     className={"bg-gray-200 w-40 font-bold text-black rounded-l-xl"}
-                    onClick={() => setview("map")}>
+                    onClick={() => {
+                        setview("map")
+                        window.scrollTo(0, 0)
+                    }}>
                     Carte
                 </button>
                 <button
@@ -25,7 +47,30 @@ export default function ListView({setview}) {
                     Liste
                 </button>
             </div>
-            <h1>List</h1>
+            {Array.isArray(events) && events?.map(event => {
+                return <div className={"mx-5 my-10"}>
+                    <img src={event.image ? event.image : musicPlaceholder} className="h-[200px] m-auto" alt="logo"/>
+                    <h2 className={"font-bold text-2xl"}>{event.name}</h2>
+                    <p className={"text-gray-400 "}>Lieu</p>
+                    <span className={"font-bold flex flex-row text-lg"}>
+                        <p className={"border-orange-500 outline-4"}>début - fin</p>
+                        <p className={"text-gray-300 mx-1"}>|</p>
+                        <p className={"text-orange-500"}>{event.price ? event.price + ' €' : 'Gratuit'}</p>
+                    </span>
+                </div>
+            })}
+            {Array.isArray(events) && events?.map(event => {
+                return <div className={"mx-5 my-10"}>
+                    <img src={event.image ? event.image : musicPlaceholder} className="h-[200px] m-auto" alt="logo"/>
+                    <h2 className={"font-bold text-2xl"}>{event.name}</h2>
+                    <p className={"text-gray-400 "}>Lieu</p>
+                    <span className={"font-bold flex flex-row text-lg"}>
+                        <p className={"border-orange-500 outline-4"}>début - fin</p>
+                        <p className={"text-gray-300 mx-1"}>|</p>
+                        <p className={"text-orange-500"}>{event.price ? event.price + ' €' : 'Gratuit'}</p>
+                    </span>
+                </div>
+            })}
         </div>
     );
 };
